de1df3b990baf7d5ccb3520dd4311fb7e567ed94
saxon drilling <saxon@saxondrilling.com>
2023-02-22 16:25:24 +0330

    map completed and contact me done

--- Tarnasi/wwwroot/js/custom.js
+++ Tarnasi/wwwroot/js/custom.js
@@ -31,6 +31,16 @@
     });  
 
     // TOOLTIP
-    $('.social-links a').tooltip();
+    // $('.social-links a').tooltip();
 
+    // leaflet map
+    var map = L.map('leafMap').setView([35.840303, 50.971692], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([35.840303, 50.971692]).addTo(map)
+        .bindPopup('(Office): Iran, Alborz, Karaj, Gohardasht')
+        .openPopup();
 })(jQuery);
